88f499be1b5dbc51d4496ff79f3fc1c79827fffe
Mohamed Mounir <m.mounir.f@gmail.com>
2019-10-17 19:08:39 +0200

    deploy cloud function

---
+++ functions/index.js
@@ -0,0 +1,28 @@
+const functions = require('firebase-functions');
+const cors = require('cors')({ origin: true});
+const admin = require('firebase-admin');
+const serviceAccount = require('./service-account.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const { SessionsClient } = require('dialogflow');
+
+
+exports.dialogflowGateway = functions.https.onRequest((request, response) => {
+  cors(request, response, async () => {
+    const { queryInput, sessionId } = request.body;
+
+
+    const sessionClient = new SessionsClient({ credentials: serviceAccount  });
+    const session = sessionClient.sessionPath('your-project', sessionId);
+
+
+    const responses = await sessionClient.detectIntent({ session, queryInput});
+
+    const result = responses[0].queryResult;
+
+    response.send(result);
+  });
+});
